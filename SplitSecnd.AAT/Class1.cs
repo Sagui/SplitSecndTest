08c0d02b04b58a69bc5c600a1068f4c5b4d21807
sagui <tativampire@hotmail.com>
2014-09-04 16:03:50 -0500

    test

--- SplitSecnd.AAT/Class1.cs
+++ SplitSecnd.AAT/Class1.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Machine.Specifications;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Support.UI;
+using JQSelenium;
 namespace SplitSecnd.AAT
 {
-    public class Class1
+  
+
+    public class when_logging_in_purshase_page
     {
-    }
+        //testing sjndjnfjfnjf 
+         
+        private Establish context =
+            () =>
+            {
+                var driver = new FirefoxDriver();
+                driver.Navigate().GoToUrl("https://google.com/");
+                var jqf = new JQuery(driver);
+            };
+
+        private Because of =
+            () => { };
+
+        private It should_log_in =
+            () => { };
+    }              
 }
